f574b22cb187f73a159124dd93842bb6f3e9ed5d
AUcod3r <webmaster@code320.com>
2020-03-13 15:54:07 -0500

    Added code to track day-to-day changes

--- covid19.py
+++ covid19.py
@@ -13,6 +13,72 @@ import datetime as dt
 import os
 
 
+def readData():
+    """
+    Read yesterday's data from the covidData text file.
+
+    This code will grab the last line for daily calculations and deltas.
+    """
+    with open('covidData.txt', 'r') as fRead:
+        for line in fRead:
+            pass
+        last = line.split(' ')
+        yestCaseNum = int(last[10].replace(',', ''))
+        yestActive = int(last[16].replace(',', ''))
+        yestClosed = int(last[20].replace(',', ''))
+        yestRecover = int(last[24].replace(',', ''))
+        yestDeaths = int(last[29].replace(',', ''))
+        return yestCaseNum, yestActive, yestClosed, yestRecover, yestDeaths
+
+
+def calDeltas(yCNum, yANum, yClNum, yRNum, yDNum, todayValues):
+    intVals = []
+    for i in range(len(todayValues)):
+        # Convert string values to intergers:
+        intVals.append(int(todayValues[i].replace(',', '')))
+
+    caseNumDiff = intVals[0] - yCNum
+    if intVals[0] > yCNum:
+        print(f'The total number of cases increased by {caseNumDiff}'
+              f' since yesterday')
+    elif caseNumDiff == 0:
+        print('Hallelujah, no additional cases!')
+    else:
+        print(
+            f'Hopefully, we have turned the corner. There are {abs(caseNumDiff)}'
+            f' less cases today!')
+
+    caseActiveDiff = intVals[3] - yANum
+    if intVals[3] > yANum:
+        print(f'Active cases increased by {caseActiveDiff} people')
+    elif caseActiveDiff == 0:
+        print('Hallelujah, no additional active cases!')
+    else:
+        print(
+            f'Hopefully, we have turned the corner. There are {abs(caseActiveDiff)}'
+            f' less active cases today!')
+
+    caseDeathDiff = intVals[1] - yDNum
+    if caseDeathDiff > 1:
+        print(f'Unfortunately, {caseDeathDiff} people died yesterday')
+    elif caseDeathDiff == 1:
+        print(f'Unfortunately, {caseDeathDiff} person died yesterday')
+    else:
+        print('Hallelujah, no additional deaths!')
+
+    caseClosedDiff = intVals[4] - yClNum
+    if caseClosedDiff > 0:
+        print(f'Closed cases increased by {caseClosedDiff}')
+    else:
+        print('No change in closed cases...')
+
+    caseRecoveredDiff = intVals[2] - yRNum
+    if caseRecoveredDiff > 0:
+        print(f'{caseRecoveredDiff} people recovered!!!')
+    else:
+        print('No change in recovered cases...')
+
+
 def writeData():
     """
     Append the data to covidData.txt.
@@ -53,6 +119,13 @@ for cases in soup.find_all('div', class_='col-md-6'):
 # Print to the terminal
 for i in range(len(labels)):
     print(labels[i], values[i])
+print()
+# Read yeseterday's data
+yCNum, yANum, yClNum, yRNum, yDNum = readData()
+
+# Caluculate and print the deltas
+calDeltas(yCNum, yANum, yClNum, yRNum, yDNum, values)
+
 # Call the function to write today's data
 writeData()
 # Print a nice little divider
